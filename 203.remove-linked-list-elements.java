7623836b421cf2dd5b7a13e75b91dce2d1eddc02
Triet TRAN <t.triet.1102@gmail.com>
2022-04-17 21:56:29 +0200

    203

---
+++ 203.remove-linked-list-elements.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=203 lang=java
+ *
+ * [203] Remove Linked List Elements
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        ListNode tmp = head;
+        while (tmp != null && tmp.next != null) {
+            if (tmp.next.val == val) {
+                tmp.next = tmp.next.next;
+            } else {
+                tmp = tmp.next;
+            }
+        }
+        return head;
+    }
+}
+// @lc code=end
+
