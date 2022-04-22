df315bbfff6dae8ed170b610f46e24f10a719859
Triet TRAN <t.triet.1102@gmail.com>
2022-04-22 23:47:50 +0200

    28

---
+++ 28.implement-str-str.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=28 lang=java
+ *
+ * [28] Implement strStr()
+ */
+
+// @lc code=start
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int needle_len = needle.length();
+        if (needle_len == 0) {
+            return 0;
+        }
+        int haystack_len = haystack.length();
+        // if (needle_len == haystack_len) {
+        //     return haystack.equals(needle) ? 0 : 1;
+        // }
+        
+        for (int i = 0; i <= (haystack_len - needle_len); i++) {
+            String sub = haystack.substring(i, i + needle_len);
+            if (sub.equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
+
